bbd4c420f0fe24bb6edbd4e45d14a90159af4f0e
arupay <arupay11374@gmail.com>
2022-04-21 14:15:41 -0400

    movie info autofill complete

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
-fetch("https://ghibliapi.herokuapp.com/films")
+const URL = "https://ghibliapi.herokuapp.com/films";
+
+fetch(URL)
   .then((response) => {
     return response.json();
   })
   .then((films) => {
+    console.log(films);
     populateTitles(films);
+    filmInfoByOptions(films);
   })
   .catch((error) => {
     console.log(error);
@@ -16,5 +20,22 @@ populateTitles = (filmObj) => {
     option.textContent = film.title;
     option.value = film.id;
     select.append(option);
+  });
+};
+
+filmInfoByOptions = (filmObj) => {
+  const select = document.getElementById("titles");
+  const displayInfo = document.getElementById("display-info");
+  select.addEventListener("change", (event) => {
+    event.preventDefault();
+    displayInfo.textContent = "";
+    let film = filmObj.find(({ id }) => id === event.target.value);
+    let title = document.createElement("h3");
+    title.textContent = film.title;
+    let year = document.createElement("p");
+    year.textContent = film.release_date;
+    let info = document.createElement("p");
+    info.textContent = film.description;
+    displayInfo.append(title, year, info);
   });
 };
