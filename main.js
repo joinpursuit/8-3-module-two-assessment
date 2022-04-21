5cb916fde8d35f0ef17bbb6d641f58659a6e378e
Jonathan Scheiber <jon@scheiber.me>
2022-04-21 11:35:15 -0400

    add movie details

--- main.js
+++ main.js
@@ -2,12 +2,29 @@ fetch("https://ghibliapi.herokuapp.com/films")
   .then((response) => response.json())
   .then((films) => {
     const titles = document.querySelector("#titles");
-    for (let film of films) {
+    for (const film of films) {
       const option = document.createElement("option");
       option.textContent = film.title;
       option.setAttribute("value", film.title);
       titles.append(option);
     }
+
+    let selectedFilm;
+    const displayInfo = document.querySelector("#display-info");
+
+    titles.addEventListener("change", () => {
+      for (const film of films) {
+        const filmReleasedDate = film.release_date;
+        const filmDescription = film.description;
+
+        if (titles.value === film.title) {
+          selectedFilm = film;
+          displayInfo.innerHTML = `<h3>${film.title}</h3>
+            <p id="film-released-date">${filmReleasedDate}</p>
+            <p id="film-description">${filmDescription}</p>`;
+        }
+      }
+    });
   })
   .catch((error) => {
     console.log(error);
