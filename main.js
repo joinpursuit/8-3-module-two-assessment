bd4e2f6ddb489fd93f5acce493bd5fbbcfb52dde
JC-MT <jancarlosmatias@gmail.com>
2022-04-21 13:26:03 -0400

    movie description feature completed

--- main.js
+++ main.js
@@ -1,17 +1,30 @@
 fetch('https://ghibliapi.herokuapp.com/films')
-.then( (response) => {
+  .then((response) => {
     return response.json();
-}).then( (response) => {
-    console.log(response)
-    getFilms(response)
-})
+  })
+  .then((response) => {
+    getFilms(response);
+  });
 
 const getFilms = (response) => {
-    const titles = document.getElementById("titles")
-    response.forEach( film => {
-        const option = document.createElement("option")
-        option.setAttribute('value', film.id)
-        option.textContent = film.title
-        titles.append(option)
-    });
-}
+  const titles = document.getElementById('titles');
+  response.forEach((film) => {
+    const option = document.createElement('option');
+    option.setAttribute('value', film.id);
+    option.textContent = film.title;
+    titles.append(option);
+  });
+
+  titles.addEventListener('change', (event) => {
+    event.preventDefault();
+    const match = response.find((film) => film.id === event.target.value);
+
+    const title = document.getElementById('title');
+    const year = document.getElementById('year');
+    const description = document.getElementById('description');
+
+    title.textContent = match.title;
+    year.textContent = match.release_date;
+    description.textContent = match.description;
+  });
+};
