82e78d27e8f96952ace84b08e43601515533f765
lchen6283 <lchen6283@gmail.com>
2022-04-21 16:25:49 -0400

    got the release year to appear but need to match it with the film

--- myscripts.js
+++ myscripts.js
@@ -5,36 +5,57 @@ let film = document.getElementById("titles");
 
 function getTitles(movies) {
   let title = new Set();
+
   for (let movie of movies) {
     title.add(movie.title);
   }
-  const dropdown = document.getElementById("titles");
+  //   const film = document.getElementById("titles");
 
   for (let titles of title) {
     const option = document.createElement("option");
     option.textContent = titles;
     option.value = titles;
 
-    dropdown.append(option);
+    film.append(option);
 
-    film.addEventListener("click", (event) => {
-      event.preventDefault;
-      h3_Title.innerHTML = film.value;
-    });
+    // const info = ({ title, release_date }) => {
+    //   film.addEventListener("click", (event) => {
+    //     event.preventDefault;
+    //     h3_Title.innerHTML = film.value;
+    //     p_Year.innerHTML = release_date;
+    //   });
+    // };
   }
   return [...title];
 }
 
-// function getYear(date) {
+function getInfo(years) {
+  let release_date = new Set();
 
-// }
+  for (let year of years) {
+    release_date.add(year.release_date);
+  }
+  for (let date of release_date) {
+    //     p_Year.textContent = date;
+    //   }
+
+    //   return [...release_date];
+
+    film.addEventListener("click", (event) => {
+      event.preventDefault;
+      h3_Title.innerHTML = film.value;
+
+      p_Year.innerHTML = date;
+    });
+  }
+}
 
 fetch("https://ghibliapi.herokuapp.com/films/")
   .then((response) => response.json())
 
   .then((json) => {
     getTitles(json);
-    // getYear(json);
+    getInfo(json);
   })
 
   .catch((error) => console.log(error));
