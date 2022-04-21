266416c0158f00724313ce5360111535773b0026
Abdullah-Tasfiek <atasfiek.ece@gmail.com>
2022-04-21 13:58:02 -0400

    completed movie description feature

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ fetch(`${BASE_URL}${format}/${id}`)
   .then((Response) => Response.json())
   .then((data) => {
     populateSelectBoxWithMovieTitles(data);
+    movieDescription(data);
   })
   .catch((error) => {
     console.log(error);
@@ -19,4 +20,32 @@ function populateSelectBoxWithMovieTitles(data) {
     option.value = data[i].release_date;
     selectBox.add(option);
   }
+}
+
+function movieDescription(data) {
+  const selectBox = document.getElementById("movieTitles");
+  const h3 = document.createElement("h3");
+  const releaseYear = document.createElement("p");
+  const movieDescription = document.createElement("p");
+  const movieDetailsSection = document.getElementById("movieDetails");
+  const div = document.getElementById("display-info");
+
+  /** https://www.codegrepper.com/code-examples/javascript/javascript+event+listener+on+select+option */
+  selectBox.addEventListener("change", function () {
+    for (let i = 0; i < data.length; i++) {
+      if (this.value === data[i].release_date) {
+        movieDetailsSection.append(h3);
+        h3.innerHTML = data[i].title;
+        div.append(h3);
+
+        movieDetailsSection.append(releaseYear);
+        releaseYear.innerHTML = data[i].release_date;
+        div.append(releaseYear);
+
+        movieDetailsSection.append(movieDescription);
+        movieDescription.innerHTML = data[i].description;
+        div.append(movieDescription);
+      }
+    }
+  });
 }
