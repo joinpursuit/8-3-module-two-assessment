be5821987100eda9f43afe4fdab81f9299954f29
JC-MT <jancarlosmatias@gmail.com>
2022-04-21 12:17:14 -0400

    created getFilms to dynamically add films to DOM

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
-
 fetch('https://ghibliapi.herokuapp.com/films')
 .then( (response) => {
     return response.json();
 }).then( (response) => {
     console.log(response)
+    getFilms(response)
 })
+
+const getFilms = (response) => {
+    const titles = document.getElementById("titles")
+    response.forEach( film => {
+        const option = document.createElement("option")
+        option.setAttribute('value', film.id)
+        option.textContent = film.title
+        titles.append(option)
+    });
+}
