ad398ad40fe03dd8dbe09d6a27ebbf746dcb6fac
lchen6283 <lchen6283@gmail.com>
2022-04-21 17:34:03 -0400

    added description section appears but need to match film

--- myscripts.js
+++ myscripts.js
@@ -9,7 +9,6 @@ function getTitles(movies) {
   for (let movie of movies) {
     title.add(movie.title);
   }
-  //   const film = document.getElementById("titles");
 
   for (let titles of title) {
     const option = document.createElement("option");
@@ -35,9 +34,17 @@ function getInfo(years) {
   for (let year of years) {
     release_date.add(year.release_date);
   }
+
   for (let date of release_date) {
-    //     p_Year.textContent = date;
-    //   }
+    // let object = {};
+    // object[date.title] = date.release_date;
+    // date = object[date.release_date];
+
+    // const option2 = document.createElement("option");
+    // option2.textContent = date;
+    // option2.value = date;
+
+    // p_Year.append(option2);
 
     //   return [...release_date];
 
@@ -50,12 +57,28 @@ function getInfo(years) {
   }
 }
 
+function getDes(des) {
+  let description = new Set();
+
+  for (let descrip of des) {
+    description.add(descrip.description);
+  }
+  for (let info of description) {
+    film.addEventListener("click", (event) => {
+      event.preventDefault;
+
+      p_Des.innerHTML = info;
+    });
+  }
+}
+
 fetch("https://ghibliapi.herokuapp.com/films/")
   .then((response) => response.json())
 
   .then((json) => {
     getTitles(json);
     getInfo(json);
+    getDes(json);
   })
 
   .catch((error) => console.log(error));
