fde47d17584efc49f0166ddd83fe5049ef1a8098
H.C.Guerrero <clydeguerrero1@gmail.com>
2022-04-21 16:59:33 -0300

    selection box implemented, no additional tests passed. Timeout Cypress Response?

--- main.js
+++ main.js
@@ -1,8 +1,39 @@
 const form = document.querySelector('form');
 let BASE_URL = `https://ghibliapi.herokuapp.com`;
-let PATH = `/films/`;
-let USER_INPUT = '';
+let PATH = `/films`;
 let API_FORMAT = `?format=j1`;
+let function_URL = `https://ghibliapi.herokuapp.com/films/`;
+
+fetch(`${BASE_URL}${PATH}${API_FORMAT}`)
+  .then((result) => result.json())
+  .then(generateSelectMenu)
+  .then(masterFunction)
+  .catch(errorHandle);
+
+/*let dummy = `https://ghibliapi.herokuapp.com/films?format = j1`;
+for (let i = 0; i < result.length; i++) {
+  let dummy2 = dummy.title[i];
+  console.log(dummy2);
+} */
+
+function masterFunction() {
+  addHeaderLogo();
+  //generateSelectMenu();
+}
+console.log(`${BASE_URL}${PATH}${API_FORMAT}`);
+
+function generateSelectMenu(json) {
+  //console.log(json);
+
+  for (i = 0; i < json.length; i++) {
+    console.log(json[i]);
+    let ghibliTitles = document.createElement('option');
+    ghibliTitles.textContent = json[i].title;
+    ghibliTitles.value = json[i].id;
+    let dropdown = document.querySelector('#Selection ');
+    dropdown.append(ghibliTitles);
+  }
+}
 
 function addHeaderLogo() {
   let header = document.querySelector('#header');
@@ -11,4 +42,27 @@ function addHeaderLogo() {
   ghibliLogo.alt = `Ghibli logo`;
   header.prepend(ghibliLogo);
 }
-addHeaderLogo();
+
+/*function generateSelectMenu(titlesArr) {
+  let titles = {};
+
+  for (obj of titlesArr) {
+    if (obj.title) {
+      //titles.push(obj.title);
+      titles[obj.title] = obj.title.value;
+    }
+  }
+  for (i = 0; i < json.length; i++) {
+    let selectTitle = [];
+    selectTitle[i] = json.title[i];
+    console.log(selectTitle[i]);
+    return selectTitle;
+  } 
+  console.log(titles);
+  return titles;
+} */
+
+function errorHandle(error) {
+  console.log(error);
+  return error;
+}
