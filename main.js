cba1a16b1f3f3995fa457c3276842eed2a37099e
JuHaoChen1997 <juhaochen97@gmail.com>
2022-04-21 12:34:15 -0400

    finish select menu

--- main.js
+++ main.js
@@ -1,1 +1,36 @@
+const BASE_URL = "https://ghibliapi.herokuapp.com/";
+const selectMenu = document.querySelector("section select");
 
+generateSelectMenu(BASE_URL, selectMenu);
+
+function generateSelectMenu(BASE_URL, selectMenu) {
+  fetch(`${BASE_URL}films`)
+    .then((response) => {
+      return response.json();
+    })
+    .then((json) => {
+      let movies = getMoviesTitleAndId(json);
+      generateSelectMenuOptions(movies, selectMenu);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+function getMoviesTitleAndId(movies) {
+  return movies.map((movie) => {
+    return {
+      title: movie.title,
+      id: movie.id,
+    };
+  });
+}
+
+function generateSelectMenuOptions(movies, selectMenu) {
+  movies.forEach((movie) => {
+    let option = document.createElement("option");
+    selectMenu.append(option);
+    option.textContent = movie.title;
+    option.value = movie.id;
+  });
+}
