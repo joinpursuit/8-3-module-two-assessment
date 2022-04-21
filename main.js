bb1a87d854ae559d70024348e218e13d478fe32d
H.C.Guerrero <clydeguerrero1@gmail.com>
2022-04-21 20:55:56 -0300

    Movie Descriptions working, no tests passed.

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const form = document.querySelector('form');
+//const form = document.querySelector('form');
 let BASE_URL = `https://ghibliapi.herokuapp.com`;
 let PATH = `/films`;
 let API_FORMAT = `?format=j1`;
@@ -6,18 +6,14 @@ let function_URL = `https://ghibliapi.herokuapp.com/films/`;
 
 fetch(`${BASE_URL}${PATH}${API_FORMAT}`)
   .then((result) => result.json())
-  .then(generateSelectMenu)
   .then(masterFunction)
   .catch(errorHandle);
 
-/*let dummy = `https://ghibliapi.herokuapp.com/films?format = j1`;
-for (let i = 0; i < result.length; i++) {
-  let dummy2 = dummy.title[i];
-  console.log(dummy2);
-} */
-
-function masterFunction() {
+function masterFunction(json) {
   addHeaderLogo();
+  generateSelectMenu(json);
+  generateMovieDescription(json);
+  makeReview(json);
   //generateSelectMenu();
 }
 console.log(`${BASE_URL}${PATH}${API_FORMAT}`);
@@ -26,13 +22,56 @@ function generateSelectMenu(json) {
   //console.log(json);
 
   for (i = 0; i < json.length; i++) {
-    console.log(json[i]);
+    // console.log(json[i]);
     let ghibliTitles = document.createElement('option');
     ghibliTitles.textContent = json[i].title;
     ghibliTitles.value = json[i].id;
     let dropdown = document.querySelector('#Selection ');
     dropdown.append(ghibliTitles);
   }
+  console.log(document.querySelector(`#Selection`).length);
+}
+
+function generateMovieDescription(json) {
+  let dropSelect = document.getElementById('Selection');
+  dropSelect.addEventListener('change', (event) => {
+    event.preventDefault();
+    let selectTitle = dropSelect.value;
+    console.log(selectTitle);
+
+    for (i = 0; i < json.length; i++) {
+      if (selectTitle === json[i].id) {
+        let movieDetails = document.getElementById('display-info');
+
+        let h3MovieTitle = document.createElement('h3');
+        h3MovieTitle.textContent = json[i].title;
+        let p1MovieRelease = document.createElement('p');
+        p1MovieRelease = json[i].release_date;
+        let p2MovieDescription = document.createElement('p');
+        p2MovieDescription = json[i].description;
+
+        movieDetails.append(h3MovieTitle, p1MovieRelease, p2MovieDescription);
+        let element = document.getElementById('display-info');
+        let number = element.childNodes.length;
+        console.log(number);
+        // console.log(document.getElementById('display-info').length);
+      }
+    }
+  });
+}
+
+function makeReview(json) {
+  const form = document.getElementById('userReview');
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const userReview = event.target.review.value;
+    const reviewList = document.querySelector('ul');
+    const userReviewEntry = document.createElement('li');
+    userReviewEntry.textContent = userReview;
+    reviewList.append(userReviewEntry);
+    console.log(userReview);
+    event.target.review.value = '';
+  });
 }
 
 function addHeaderLogo() {
@@ -42,25 +81,6 @@ function addHeaderLogo() {
   ghibliLogo.alt = `Ghibli logo`;
   header.prepend(ghibliLogo);
 }
-
-/*function generateSelectMenu(titlesArr) {
-  let titles = {};
-
-  for (obj of titlesArr) {
-    if (obj.title) {
-      //titles.push(obj.title);
-      titles[obj.title] = obj.title.value;
-    }
-  }
-  for (i = 0; i < json.length; i++) {
-    let selectTitle = [];
-    selectTitle[i] = json.title[i];
-    console.log(selectTitle[i]);
-    return selectTitle;
-  } 
-  console.log(titles);
-  return titles;
-} */
 
 function errorHandle(error) {
   console.log(error);
