196b5c3e8564d8558f428f2a7c3d5f8780204e91
PratimaRoy <pratimaroy170@gmail.com>
2022-04-21 19:11:28 -0400

    Add reviews works but Select a Menu keeps showing x when it functions

--- main.js
+++ main.js
@@ -1,34 +1,76 @@
 const dropDown = document.getElementById('movie-dropdown');
 const info = document.getElementById('display-info');
+const ul = document.querySelector('ul');
+const generatePeople = document.getElementById('show-people')
 const reviewForm = document.querySelector('form');
+
+
 //Fetch API Ghibli data
 
 fetch('https://ghibliapi.herokuapp.com/films')
 .then((response) => response.json())
 .then((movies) => {
+    getMovieInfo(movies);
+    getReviews(movies);
+})
+.catch((e) => {
+    console.log(e);
+});
+
+const getMovieInfo = (movies) => {
     movies.forEach((movie) => {
         const option = document.createElement('option');
-        option.setAttribute('value', movie.id);
+        option.value = movie.id;
         option.textContent = movie.title;
         dropDown.append(option);
- 
-        dropDown.addEventListener('click', (event) => {
-            event.preventDefault();
-            movies.forEach((movie) => {
-                if (event.target.value === movie.id) {
-                    info.textContent = '';
+    });
+
+       dropDown.addEventListener('change', (event) => {
+                    event.preventDefault();
+                    const userChoose = event.target.value;
+                    for (let movie of movies) {
+                        if (userChoose === movie.id) {
+                            info.textContent = '';
                     const h3 = document.createElement('h3');
-                info.prepend(h3);
-            h3.textContent = movie.title;
-            const p1 = document.createElement('p');
-            info.append(p1);
-            p1.textContent = movie.release_date;
-            
-            const p2 = document.createElement('p');
-            info.append(p2);
-            p2.textContent = movie.description;  
-     }
-   });
-  });
- });
-});
+                    info.prepend(h3);
+                    h3.textContent = movie.title;
+                    const p1 = document.createElement('p');
+                    info.append(p1);
+                    p1.textContent = movie.release_date;
+                    
+                    const p2 = document.createElement('p');
+                    info.append(p2);
+                    p2.textContent = movie.description;  
+            }
+           };
+          });
+        };
+
+    function getReviews(movies) {
+        reviewForm.addEventListener('submit', (event) => {
+            event.preventDefault();
+            let reviewInput = document.getElementById('review').value;
+            if (dropDown.value === '') {
+                alert('Please select a movie first')
+            } else {
+                let movie = movies.find((movie) => movie.id === dropDown.value);
+                let li = document.createElement('li')
+                li.innerHTML = `<strong>${movie.title}: </strong>${reviewInput}`;
+                ul.append(li);
+            }
+            reviewForm.reset();
+        });
+    }
+
+    const resetReviewButton = document.getElementById('reset-reviews');
+    resetReviewButton.addEventListener('click', (event) => {
+        event.preventDefault();
+        ul.innerHTML = '';
+    });
+
+    function getPeople(movies) {
+        generatePeople.addEventListener('click', (event) => {
+            event.preventDefault();
+        })
+    }
+
