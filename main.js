6118646fc0625b54fb2f8c1d98f467c9fadc0937
Jonathan Scheiber <jon@scheiber.me>
2022-04-21 11:23:17 -0400

    add films to titles dropdown

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 fetch("https://ghibliapi.herokuapp.com/films")
   .then((response) => response.json())
-  .then((films) => {})
+  .then((films) => {
+    const titles = document.querySelector("#titles");
+    for (let film of films) {
+      const option = document.createElement("option");
+      option.textContent = film.title;
+      option.setAttribute("value", film.title);
+      titles.append(option);
+    }
+  })
   .catch((error) => {
     console.log(error);
   });
