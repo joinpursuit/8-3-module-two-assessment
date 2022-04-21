d252d9cfb2ea80943c3db071cc38ac1feca3e078
JuHaoChen1997 <juhaochen97@gmail.com>
2022-04-21 14:01:36 -0400

    finish movie description

--- main.js
+++ main.js
@@ -1,36 +1,65 @@
 const BASE_URL = "https://ghibliapi.herokuapp.com/";
 const selectMenu = document.querySelector("section select");
+const movieDetails = document.getElementById("display-info");
 
-generateSelectMenu(BASE_URL, selectMenu);
+generateWebPage(BASE_URL, selectMenu, movieDetails);
 
-function generateSelectMenu(BASE_URL, selectMenu) {
+function generateWebPage(BASE_URL, selectMenu, movieDetails) {
   fetch(`${BASE_URL}films`)
     .then((response) => {
       return response.json();
     })
     .then((json) => {
-      let movies = getMoviesTitleAndId(json);
-      generateSelectMenuOptions(movies, selectMenu);
+      let moviesData = parseJsonData(json);
+      generateSelectMenu(moviesData, selectMenu);
+      return moviesData;
+    })
+    .then((moviesData) => {
+      generateMovieDescription(moviesData, selectMenu, movieDetails);
     })
     .catch((error) => {
       console.log(error);
     });
 }
 
-function getMoviesTitleAndId(movies) {
+function parseJsonData(movies) {
   return movies.map((movie) => {
     return {
       title: movie.title,
       id: movie.id,
+      release_date: movie.release_date,
+      description: movie.description,
     };
   });
 }
 
-function generateSelectMenuOptions(movies, selectMenu) {
-  movies.forEach((movie) => {
+//generate select menu
+function generateSelectMenu(moviesData, selectMenu) {
+  moviesData.forEach((movie) => {
     let option = document.createElement("option");
     selectMenu.append(option);
     option.textContent = movie.title;
     option.value = movie.id;
+  });
+}
+
+//generate movie description
+function generateMovieDescription(moviesData, selectMenu, movieDetails) {
+  selectMenu.addEventListener("change", (event) => {
+    event.preventDefault();
+    movieDetails.innerHTML = "";
+    let title = document.createElement("h3");
+    let release_year = document.createElement("p");
+    let description = document.createElement("p");
+
+    moviesData.forEach((movie) => {
+      if (selectMenu.value === movie.id) {
+        title.textContent = movie.title;
+        release_year.textContent = movie.release_date;
+        description.textContent = movie.description;
+      }
+    });
+
+    movieDetails.append(title, release_year, description);
   });
 }
