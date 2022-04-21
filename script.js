a89192d4de02f3ca2c4b8c4fd01cee0a7de106b7
EmaleeSoto <emaleesoto@gmail.com>
2022-04-21 10:37:10 -0400

    Created file, added TODOs for future behavior

---
+++ script.js
@@ -0,0 +1,5 @@
+//TODO: Popluate selection options with movies from API
+//TODO: Fix blank default value for select
+//TODO: Populate div with h3 and 2 p elements when movie is selected
+//TODO: Populate ol with li names of people, button should show people (maybe toggle visbility of list)
+//TODO: Populate ul with li reviews, button should reset text to blank
