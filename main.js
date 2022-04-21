d0e0abb746b6e315965a8ee8a4402dd510bfd88a
JC-MT <jancarlosmatias@gmail.com>
2022-04-21 14:55:47 -0400

    adding/removing review feature completed

--- main.js
+++ main.js
@@ -7,24 +7,51 @@ fetch('https://ghibliapi.herokuapp.com/films')
   });
 
 const getFilms = (response) => {
-  const titles = document.getElementById('titles');
-  response.forEach((film) => {
-    const option = document.createElement('option');
-    option.setAttribute('value', film.id);
-    option.textContent = film.title;
-    titles.append(option);
-  });
+    const titles = document.getElementById('titles')
 
-  titles.addEventListener('change', (event) => {
-    event.preventDefault();
-    const match = response.find((film) => film.id === event.target.value);
+    response.forEach((film) => {
+        const option = document.createElement('option');
+        option.setAttribute('value', film.id);
+        option.textContent = film.title;
+        titles.append(option);
+    });
 
-    const title = document.getElementById('title');
-    const year = document.getElementById('year');
-    const description = document.getElementById('description');
+    titles.addEventListener('change', (event) => {
+        event.preventDefault();
+        const match = response.find((film) => film.id === event.target.value);
 
-    title.textContent = match.title;
-    year.textContent = match.release_date;
-    description.textContent = match.description;
-  });
+        const title = document.getElementById('title');
+        const year = document.getElementById('year');
+        const description = document.getElementById('description');
+
+        title.textContent = match.title;
+        year.textContent = match.release_date;
+        description.textContent = match.description;
+    });
 };
+
+const reviewForm = document.querySelector('form')
+
+reviewForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    if (title.textContent === ''){
+        alert("Please select a movie first")
+    } else {
+        const text = document.getElementById('review')
+        const reviewList = document.querySelector('ul')
+        const review = document.createElement('li')
+        
+        review.innerHTML = `<strong>${title.textContent}: </strong><span>${text.value}</span>`
+    
+        reviewList.append(review)
+    }
+    reviewForm.reset()
+})
+
+const resetButton = document.getElementById('reset-reviews')
+
+resetButton.addEventListener('click', (event) => {
+    event.preventDefault()
+    const reviewList = document.querySelector('ul')
+    reviewList.innerHTML = ''
+})
