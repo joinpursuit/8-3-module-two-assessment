78a62c5cb18f6bbdf9bb7c9a50fc0972ceca69d6
Isaac Gonzalez <u1337x@gmail.com>
2022-04-21 15:36:11 -0400

    maybe second suite

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-const select = document.querySelector("#SelectTag");
+const selected = document.getElementById("select");
+
+fetch(`https://ghibliapi.herokuapp.com/films`)
+  .then((response) => response.json())
+  .then((json) => {
+    parse(json);
+  })
+  .catch((error) => console.log(error));
+
+function parse(file) {
+  let arr = [];
+  for (const current of file) {
+    arr.push(current.title);
+  }
+  for (let i = 0; i < arr.length; i++) {
+    let option = document.createElement("option");
+    selected.appendChild(option);
+    option.value = arr[i];
+    option.text = arr[i];
+    selected.appendChild(option);
+  }
+}
