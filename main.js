af5bd429325b14f4290dafea218839222e2aec43
H.C.Guerrero <clydeguerrero1@gmail.com>
2022-04-22 10:38:38 -0300

    generateMovieDescription reworked.

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
-//const form = document.querySelector('form');
 let BASE_URL = `https://ghibliapi.herokuapp.com`;
 let PATH = `/films`;
 let API_FORMAT = `?format=j1`;
-let function_URL = `https://ghibliapi.herokuapp.com/films/`;
 
 fetch(`${BASE_URL}${PATH}${API_FORMAT}`)
   .then((result) => result.json())
@@ -15,22 +13,18 @@ function masterFunction(json) {
   generateMovieDescription(json);
   makeReview(json);
   resetReviews();
-  //generateSelectMenu();
 }
-console.log(`${BASE_URL}${PATH}${API_FORMAT}`);
 
 function generateSelectMenu(json) {
-  //console.log(json);
-
   for (i = 0; i < json.length; i++) {
-    // console.log(json[i]);
     let ghibliTitles = document.createElement('option');
     ghibliTitles.textContent = json[i].title;
     ghibliTitles.value = json[i].id;
-    let dropdown = document.querySelector('#Selection ');
+    let dropdown = document.querySelector('#Selection');
     dropdown.append(ghibliTitles);
   }
-  console.log(document.querySelector(`#Selection`).length);
+  let dropDownLengthCheck = document.querySelector(`#Selection`).length;
+  console.log(dropDownLengthCheck);
 }
 
 function generateMovieDescription(json) {
@@ -38,54 +32,59 @@ function generateMovieDescription(json) {
   dropSelect.addEventListener('change', (event) => {
     event.preventDefault();
     let selectTitle = dropSelect.value;
-    console.log(selectTitle);
 
     for (i = 0; i < json.length; i++) {
       if (selectTitle === json[i].id) {
-        console.log(selectTitle);
-        console.log(json[i].id);
         let movieDetails = document.getElementById('display-info');
 
         let h3MovieTitle = document.createElement('h3');
         h3MovieTitle.textContent = json[i].title;
         let p1MovieRelease = document.createElement('p');
+
         p1MovieRelease = json[i].release_date;
+        let linebreak = document.createElement('br');
+
         let p2MovieDescription = document.createElement('p');
         p2MovieDescription = json[i].description;
 
-        movieDetails.append(
-          h3MovieTitle,
-          p1MovieRelease + ' ' + p2MovieDescription,
-        );
+        movieDetails.append(h3MovieTitle);
+        movieDetails.append(p1MovieRelease);
+        movieDetails.appendChild(linebreak);
+        movieDetails.append(p2MovieDescription);
 
         let element = document.getElementById('display-info');
-        let number = element.childNodes.length;
-        console.log(number);
-        // console.log(document.getElementById('display-info').length);
+        let numberOfDetailNodes = element.childNodes.length;
+        console.log(numberOfDetailNodes);
       }
     }
   });
 }
+///My attempt at making a helper function to bold only the review's movie title.
+/// Causes an infinite loop, and I didn't have time to troubleshoot the issue.
+/*function formatTitle(json) {
+    for (i = 0; i < json.length; i++) {
+      let wrapper = json[i].title;
+      let boldWrapper = wrapper.bold();
+      console.log(boldWrapper);
+      return boldWrapper;
+    }
+  } */
 
 function makeReview(json) {
   let dropSelectReview = document.getElementById('Selection');
-
   const form = document.getElementById('userReview');
   form.addEventListener('submit', (event) => {
     event.preventDefault();
     let selectTitleReview = dropSelectReview.value;
     for (i = 0; i < json.length; i++) {
-      console.log(selectTitleReview);
-      console.log(json[i].id);
       if (selectTitleReview === json[i].id) {
         const userReview = event.target.review.value;
         const reviewList = document.querySelector('ul');
         const userReviewEntry = document.createElement('li');
-        userReviewEntry.textContent = `${json[i].title}:` + ` ` + userReview;
+        userReviewEntry.textContent = `${json[i].title}.` + `  `;
         userReviewEntry.style.fontWeight = 'bold';
-
+        userReviewEntry.textContent += `${userReview}`;
         reviewList.append(userReviewEntry);
-        console.log(userReview);
         event.target.review.value = '';
       } else if (selectTitleReview === '') {
         window.alert('Please select a movie first');
@@ -98,7 +97,6 @@ function resetReviews() {
   let resetReviews = document.getElementById('reset-reviews-form');
   resetReviews.addEventListener('submit', (event) => {
     event.preventDefault();
-
     const reviewNode = document.getElementById('reviewsList');
     while (reviewNode.firstChild) {
       reviewNode.removeChild(reviewNode.lastChild);
