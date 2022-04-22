1144adb200b6a79d948d5f07285ae48d2214c138
Fig <bethanytrr@gmail.com>
2022-04-22 10:10:33 -0400

    Description ideally should work but having issue accessing eventlistener.

--- index.js
+++ index.js
@@ -1,23 +1,44 @@
-let select = document.getElementById('titles');
-let details = document.getElementById('display-info')
-const url = 'https://ghibliapi.herokuapp.com/films';
+const select = document.querySelector('section select');
+const details = document.querySelector('#display-info')
+const url = 'https://ghibliapi.herokuapp.com/';
+
+fetch(`${url}films`)
+    .then((response) => {
+        return response.json();
+    })
+    .then((movieData) => {
+        selectionBoxWithTitles(movieData);
+    })
+    .catch((error) => {
+        console.error(error)
+    });
 
 
-select.addEventListener("select", (event) => {
+function selectionBoxWithTitles(movieData) {
+    for (let movie of movieData) {
+        const select = document.querySelector('section select');
+        let option = document.createElement('option');
+        option.text = movie.title;
+        option.value = movie.release_date;
+        select.add(option);;
+    }
+}
+
+select.addEventListener('change', (event) => {
     event.preventDefault();
-    fetch(url)
-        .then((response) => {
-            return response.json();
-        })
-        .then((json) => {
-            for (let i = 0; i < json.length; i++) {
-                option = document.createElement('option');
-                option.text = json[i].title;
-                option.value = json[i].title;
-                select.add(option);
-            }
-        })
-        .catch((error) => {
-            console.error(error)
-        });
+
+    details.innerHTML = '';
+    let selected = event.target.value;
+    let titleName = document.createElement('h3')
+    let release = document.createElement('p')
+    let desc = document.createElement('p')
+
+    for (let i = 0; i < movieData.length; i++) {
+        if (selected === movie.title) {
+            titleName.textContent = movie.title;
+            release.textContent = movie.release_date;
+            desc.textContent = movie.description;
+        }
+    }
+    details.append(titleName, release, desc);
 })
