d08408f7cd88d7994eb0601850526fe82687aff0
krystalMansour <Krystalmansour@pursuit.org>
2022-04-22 01:18:31 -0400

    21 checks, Select Menu and Movie Description sections are now passing all checks

--- Ghibli.js
+++ Ghibli.js
@@ -1,7 +1,12 @@
 const BASE_URL = `https://ghibliapi.herokuapp.com/films`
+
 const selectDropDown = document.querySelector("select")
 const movieTitle = document.querySelector("div h3")
 const movieInfo = document.querySelectorAll("div p")
+const reviewSubmit = document.querySelector("form")
+const reviews = document.querySelector("ul")
+const resetButton = document.querySelector("#reset-reviews")
+let movie;
 
 fetch(BASE_URL) 
 .then((res) => res.json())
@@ -24,6 +29,8 @@ function movieList(res) {
 }
 
 selectDropDown.addEventListener("change", (event) => {  
+    event.preventDefault()
+
     movieTitle.innerText = ""
     movieInfo[0].innerText = ""
     movieInfo[1].innerText = ""
@@ -38,7 +45,34 @@ selectDropDown.addEventListener("change", (event) => {
         movieTitle.innerText = `${selectedMovie.title}`
         movieInfo[0].innerText = `${selectedMovie.release_date}`
         movieInfo[1].innerText = `${selectedMovie.description}`
+        movie = selectedMovie.title;
     })
     .catch((error) => console.log(error))
+
+    if (event.target.value === "") {
+        movie = ""
+    }
 })
 
+reviewSubmit.addEventListener("submit", (event) => {
+    event.preventDefault()
+
+    if (movie === "") {
+        alert("Please select a movie first")
+        return
+    }
+
+    let writtenReview = event.target.review.value
+
+    let rev = document.createElement("li")
+    rev.innerHTML = `<strong>${movie}:</strong> ${writtenReview}`
+    reviews.append(rev)
+
+    event.target.review.value = ""    
+})
+
+resetButton.addEventListener("click", (event) => {
+    event.preventDefault()
+
+    reviews.innerHTML = ""
+})
