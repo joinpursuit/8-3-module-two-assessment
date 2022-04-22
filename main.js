4c98e8d9281c36833311c1d4c1c23bb2f0e658f0
Stephanie Frias <steph.frias01@me.com>
2022-04-21 20:24:58 -0400

    added to js

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
-const BASE_URL = "https://ghibliapi.herokuapp.com/";
-const selectMenu = document.querySelector("section select");
+const base_URL = 'https://ghibliapi.herokuapp.com/films/';
+const people_URL = 'https://ghibliapi.herokuapp.com/people';
+const dropdown = document.getElementById('movie-dropdown');
+const info = document.getElementById('display-info');
+const reviewForm = document.querySelector('form');
+const ul = document.querySelector('ul');
+const peopleButton = document.getElementById('show-people');
 
+function generateSelectMenu(BASE_URL, selectMenu) {
+  fetch(`${BASE_URL}films`)
+    .then((response) => {
+      return response.json();
+    })
+    .then((json) => {
+      let movies = getMoviesTitleAndId(json);
+      generateSelectMenuOptions(movies, selectMenu);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
