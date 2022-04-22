17986f7c5a317f53c9b90d9d12b3c344cb6ae149
CandaceNia <98069665+CandaceNia@users.noreply.github.com>
2022-04-22 13:13:27 -0400

    .

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
-const movies_URL = "https://ghibliapi.herokuapp.com/files/";
+const movies_URL = "https://ghibliapi.herokuapp.com/films/";
 const ppl_URL = "https://ghibliapi.herokuapp.com/people/";
 const dropdownMenu = document.getElementById("movie-dropdown");
 const displayInfo = document.getElementById('display-info');
-const 
+const ul = document.querySelector('ul');
+const showPeople = document.getElementById('show-people');
+const form = document.querySelector('form');
+
 fetch(movies_URL)
   .then((res) => res.json())
-  .then((movies) => {})
+  .then((movies) => {
+      getMovieInfo(movies);
+reviewInfo (movies);
+  })
   .catch((e) => {
     console.log(e);
   });
@@ -17,4 +23,55 @@ const getMovieInfo = (movies) => {
     option.textContent = movie.title;
     dropdownMenu.append(option);
   });
-};
+  dropdownMenu.addEventListener('change', (event) => {
+      const movieId= event.target.value;
+
+      for (let movie of movies){
+
+          if( movieId === movie.id){
+              info.textContent= '';
+              const h3 = document.createElement('h3');
+              displayInfo.prepend('h3');
+              h3.textContent = movie.title;
+              
+
+              const pOne = document.createElement('p');
+              info.append(pOne);
+              pOne.textContent = movie.release_date;
+
+
+              const p2 = document.createElement('p');
+              info.append(p2);
+          }
+        };
+    });
+
+}
+    //    function reviewInfo (people)  {
+    //         // people.forEach ((person) =>{
+    //             form.addEventListener('sumbit', (event) => {
+    //                 event.preventDefault();
+    //                const reviews = document.getElementById ('review').value;
+    //                if (dropdownMenu.value === ''){
+    //                    console.log('Select a Movie')
+    //                }else {
+    //                }
+                   
+        
+       
+    //    ;
+    
+    
+                // ordered = document.createElement ('ol');
+                // ordered.textContent = person.name;
+                // ul.append(ordered)
+  
+
+
+
+    //    
+    
+
+
+
+
