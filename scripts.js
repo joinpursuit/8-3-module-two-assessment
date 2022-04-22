ce7caf55ce0ac3654c8583b2232190a88d214e7d
named.josie <named.josie@gmail.com>
2022-04-22 00:28:44 -0400

    update file

--- scripts.js
+++ scripts.js
@@ -1,20 +1,72 @@
-
- const BASE_URL = 'https://ghibliapi.herokuapp.com/films/';
-let dropdown = document.querySelector('#titles')
-  fetch(BASE_URL)
+let API_URL = 'https://ghibliapi.herokuapp.com/films/'; //API Link
+let CHAR_URL = 'https://ghibliapi.herokuapp.com/people/';
+let dropdown = document.getElementById('title');
+let display = document.getElementById('display-info');
+let reviewForm = document.querySelector('form');
+let button = document.getElementById('show-people');
+let names = document.querySelector('ol');
+let ul = document.querySelector('ul');
+fetch(API_URL)
   .then((response) => response.json())
   .then((json) => {
-    for (let e of json) {
-      const option = new Option(e.title, e.title);
-      option.textContent = e.title;
-      option.value = e.id;
-      dropdown.append(option);
+    for (let movieTitle of json) {
+    const option = new Option(movieTitle.title, movieTitle.title);
+    option.textContent = movieTitle.title;
+    option.value = movieTitle.id;
+    dropdown.append(option);
+    dropdown.addEventListener('change', (event) => {
+    event.preventDefault();
+    const identifaction = event.target.value;
+    for (let movie of json) {
+    if (identifaction === movie.id) {
+        display.textContent = '';
+        const h3 = document.createElement('h3');
+        display.prepend(h3);
+        h3.textContent = movie.title;
+        const p1 = document.createElement('p');
+        display.append(p1);
+        p1.textContent = movie.release_date;
+        const p2 = document.createElement('p');
+        display.append(p2);
+        p2.textContent = movie.description;
+          }
+        }
+        button.addEventListener('click', (event) => {
+          event.preventDefault();
+          fetch(CHAR_URL)
+            .then((response) => response.json())
+            .then((people) => {
+              people.innerHTML = '';
+              for (let person of people) {
+                for (let film of person.films) {
+                  if (film === `${API_URL}${identifaction}`) {
+                    let personList = document.createElement('li');
+                    personList.textContent = person.name;
+                    names.append(personList);
+                  }
+                }
+              }
+            });
+        });
+      });
     }
-
-
-
-
-})
+    getReviews(json);
+  })
   .catch((error) => {
     console.log(error);
   });
+function getReviews(json) {
+  reviewForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    let reviewInput = document.getElementById('review').value;
+    if (dropdown.value === '') {
+      alert('Please select a movie first');
+    } else {
+      let movie = json.find((movie) => movie.id === dropdown.value);
+      let li = document.createElement('li');
+      li.innerHTML = `<strong>${movie.title}:</strong>${reviewInput}`;
+      ul.append(li);
+    }
+    reviewForm.reset();
+  });
+}
