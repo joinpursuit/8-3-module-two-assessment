759e8ca26f68d10ce2a425ed7a5fa281dd00e974
PratimaRoy <pratimaroy170@gmail.com>
2022-04-21 21:58:59 -0400

    All the 5 tests works but the cypress for the first 2 suites act weird but it functions though

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const dropDown = document.getElementById('movie-dropdown');
 const info = document.getElementById('display-info');
 const ul = document.querySelector('ul');
 const generatePeople = document.getElementById('show-people')
+const ghibliChars = document.querySelector('ol');
 const reviewForm = document.querySelector('form');
 
 
@@ -25,52 +26,54 @@ const getMovieInfo = (movies) => {
         dropDown.append(option);
     });
 
-       dropDown.addEventListener('change', (event) => {
-                    event.preventDefault();
-                    const userChoose = event.target.value;
-                    for (let movie of movies) {
-                        if (userChoose === movie.id) {
-                            info.textContent = '';
-                    const h3 = document.createElement('h3');
-                    info.prepend(h3);
-                    h3.textContent = movie.title;
-                    const p1 = document.createElement('p');
-                    info.append(p1);
-                    p1.textContent = movie.release_date;
+dropDown.addEventListener('change', (event) => {
+event.preventDefault();
+const userChoose = event.target.value;
+for (let movie of movies) {
+if (userChoose === movie.id) {
+    info.textContent = '';
+    const h3 = document.createElement('h3');
+    info.prepend(h3);
+    h3.textContent = movie.title;
+    const p1 = document.createElement('p');
+    info.append(p1);
+    p1.textContent = movie.release_date;
                     
-                    const p2 = document.createElement('p');
-                    info.append(p2);
-                    p2.textContent = movie.description;  
-            }
-           };
-          });
-        };
+    const p2 = document.createElement('p');
+    info.append(p2);
+    p2.textContent = movie.description;  
+   }
+  };
+ });
+};
 
-    function getReviews(movies) {
-        reviewForm.addEventListener('submit', (event) => {
-            event.preventDefault();
-            let reviewInput = document.getElementById('review').value;
-            if (dropDown.value === '') {
-                alert('Please select a movie first')
-            } else {
-                let movie = movies.find((movie) => movie.id === dropDown.value);
-                let li = document.createElement('li')
-                li.innerHTML = `<strong>${movie.title}: </strong>${reviewInput}`;
-                ul.append(li);
-            }
+function getReviews(movies) {
+reviewForm.addEventListener('submit', (event) => {
+event.preventDefault();
+let reviewInput = document.getElementById('review').value;
+    if (dropDown.value === '') {
+    alert('Please select a movie first')
+    } else {
+            let movie = movies.find((movie) => movie.id === dropDown.value);
+            let li = document.createElement('li')
+            li.innerHTML = `<strong>${movie.title}: </strong>${reviewInput}`;
+            ul.append(li);
+        }
             reviewForm.reset();
         });
     }
 
-    const resetReviewButton = document.getElementById('reset-reviews');
-    resetReviewButton.addEventListener('click', (event) => {
-        event.preventDefault();
-        ul.innerHTML = '';
-    });
+const resetReviewButton = document.getElementById('reset-reviews');
+resetReviewButton.addEventListener('click', (event) => {
+event.preventDefault();
+ul.innerHTML = '';
+});
 
-    function getPeople(movies) {
-        generatePeople.addEventListener('click', (event) => {
-            event.preventDefault();
-        })
-    }
+function getPeople(movies) {
+generatePeople.addEventListener('click', (event) => {
+event.preventDefault();
+ })
+};
+
+
 
