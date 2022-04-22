12751d651579554c12c60776fe96b6ef9a01892c
named.josie <named.josie@gmail.com>
2022-04-21 21:01:38 -0400

    update file

--- scripts.js
+++ scripts.js
@@ -1,12 +1,20 @@
-BASE_URL = 'https://ghibliapi.herokuapp.com/films'
 
-fetch(BASE_URL)
-.then((response)=>response.json())
-.then(()=>{
-
-
+ const BASE_URL = 'https://ghibliapi.herokuapp.com/films/';
+let dropdown = document.querySelector('#titles')
+  fetch(BASE_URL)
+  .then((response) => response.json())
+  .then((json) => {
+    for (let e of json) {
+      const option = new Option(e.title, e.title);
+      option.textContent = e.title;
+      option.value = e.id;
+      dropdown.append(option);
+    }
 
 
 
 
 })
+  .catch((error) => {
+    console.log(error);
+  });
