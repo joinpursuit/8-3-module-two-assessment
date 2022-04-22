c51734ccf98a460ea89b9b6265477ba60b19c612
JC-MT <jancarlosmatias@gmail.com>
2022-04-22 09:15:46 -0400

    final js file clean-up: completed project

--- main.js
+++ main.js
@@ -6,65 +6,65 @@ fetch('https://ghibliapi.herokuapp.com/films')
     getFilms(response);
   });
 
+const titles = document.getElementById('titles');
+
 const getFilms = (response) => {
-  response.forEach( (film) => {
-    const option = document.createElement('option');
+    response.forEach((film) => {
+        const option = document.createElement('option');
 
-    option.setAttribute('value', film.id);
-    option.textContent = film.title;
-    titles.append(option);
-  });
-
-  const titles = document.getElementById('titles');
-  
-  titles.addEventListener('change', (event) => {
-    event.preventDefault();
-    const match = response.find((film) => film.id === event.target.value);
-
-    const title = document.getElementById('title');
-    const year = document.getElementById('year');
-    const description = document.getElementById('description');
-
-    title.textContent = match.title;
-    year.textContent = match.release_date;
-    description.textContent = match.description;
-
-    const reviewForm = document.querySelector('form');
-
-    reviewForm.addEventListener('submit', (event) => {
-      event.preventDefault();
-      if (!titles.value) {
-        window.alert('Please select a movie first');
-      } else {
-        const text = document.getElementById('review');
-        const reviewList = document.querySelector('ul');
-        const review = document.createElement('li');
-
-        review.innerHTML = `<strong>${title.textContent}: </strong><span>${text.value}</span>`;
-        reviewList.append(review);
-        reviewForm.reset();
-      }
+        option.setAttribute('value', film.id);
+        option.textContent = film.title;
+        titles.append(option);
     });
 
-    fetch(`https://ghibliapi.herokuapp.com/films/${titles.value}`)
-      .then((response) => {
-        return response.json();
-      })
-      .then((response) => {
-        const listOfPeople = document.querySelector('ol');
-        listOfPeople.innerHTML = '';
+    titles.addEventListener('change', (event) => {
+        event.preventDefault();
+        const match = response.find((film) => film.id === event.target.value);
 
-        response.people.forEach( (person) => {
-            fetch(`${person}`)
-            .then((response) => {
-              return response.json();
-            })
-            .then((response) => {
-              getPeople(response.name);
+        const title = document.getElementById('title');
+        const year = document.getElementById('year');
+        const description = document.getElementById('description');
+
+        title.textContent = match.title;
+        year.textContent = match.release_date;
+        description.textContent = match.description;
+
+        const reviewForm = document.querySelector('form');
+
+        reviewForm.addEventListener('submit', (event) => {
+            event.preventDefault();
+            if (!titles.value) {
+                window.alert('Please select a movie first');                
+            } else {
+                const text = document.getElementById('review');
+                const reviewList = document.querySelector('ul');
+                const review = document.createElement('li');
+
+                review.innerHTML = `<strong>${title.textContent}: </strong><span>${text.value}</span>`;
+                reviewList.append(review);
+                reviewForm.reset();
+            }
+        });
+
+        fetch(`https://ghibliapi.herokuapp.com/films/${titles.value}`)
+        .then((response) => {
+            return response.json();
+        })
+        .then((response) => {
+            const listOfPeople = document.querySelector('ol');
+            listOfPeople.innerHTML = '';
+
+            response.people.forEach((person) => {
+                fetch(`${person}`)
+                .then((response) => {
+                    return response.json();
+                })
+                .then((response) => {
+                    getPeople(response.name);
+                });
             });
-          });
-      });
-  });
+        });
+    });
 };
 
 const getPeople = (response) => {
