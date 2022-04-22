7ec9f3f13f606e0edc8eb310b2065542dc7ee8ec
Matt M <Afrodesiacc@gmail.com>
2022-04-21 23:11:39 -0400

    Completed 5 out of 7 sections; 'Select Box' test doesnt pass, but works.

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 let BASE_URL = 'https://ghibliapi.herokuapp.com/films/'; //API Link
+let PEOPLE_URL = 'https://ghibliapi.herokuapp.com/people/'
 
 let dropdown = document.getElementById('title')
+let info = document.getElementById('display-info')
+let reviewForm = document.querySelector('form')
+let peopleButton = document.getElementById('show-people')
+let peopleNames = document.querySelector('ol')
+let ul = document.querySelector('ul')
 
 fetch(BASE_URL) 
         .then((response) => response.json())
@@ -10,18 +16,62 @@ fetch(BASE_URL)
             option.textContent = e.title;
             option.value = e.id;
             dropdown.append(option);
+            dropdown.addEventListener('change', (event) => {
+                event.preventDefault();
+                const chosenID = event.target.value
+                for (let movie of json) {
+                  if (chosenID === movie.id) {
+                  info.textContent = ''
+                  const h3 = document.createElement('h3');
+                  info.prepend(h3);
+                  h3.textContent = movie.title;
+
+                  const p1 = document.createElement('p');
+                  info.append(p1)
+                  p1.textContent = movie.release_date;
+
+                  const p2 = document.createElement('p');
+                  info.append(p2)
+                  p2.textContent = movie.description;
+                  }
+                }
+                peopleButton.addEventListener('click', (event) => {
+                  event.preventDefault()
+                  fetch(PEOPLE_URL)
+                  .then((response) => response.json())
+                  .then((people) => {
+                    people.innerHTML = '';
+                    for (let person of people) {
+                      for (let film of person.films) {
+                        if (film === `${BASE_URL}${chosenID}`) {
+                          let personList = document.createElement('li');
+                          personList.textContent = person.name
+                          peopleNames.append(personList)
+                        }
+                      }
+                    }
+                  })
+                }) 
+            })
           }
+          getReviews(json);
         })
         .catch((error) => {
             console.log(error);
           });
 
-const displayData = (data) => {
-    console.log(data);
+function getReviews(json) {
+  reviewForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    let reviewInput = document.getElementById('review').value;
+    if (dropdown.value === '') {
+      alert('Please select a movie first');
+    } else {
+      let movie = json.find((movie) => movie.id === dropdown.value);
+      let li = document.createElement('li');
+      li.innerHTML = `<strong>${movie.title}:</strong>${reviewInput}`;
+      ul.append(li)
+    }
+    reviewForm.reset()
+  })
 }
-
-const displayError = (error) => {
-    console.log(error);
-}
-    //an event listener for user input
-    //create input element, create form element for search bar
