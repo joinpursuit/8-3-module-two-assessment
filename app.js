a913acfbaa23d428998b6c276ef201a9179de850
meera-ramesh19 <meeraramesh19@outlook.com>
2022-04-22 00:33:33 -0400

    get people name test passed

--- app.js
+++ app.js
@@ -1,46 +1,114 @@
+const showPeopleNames = (
+  BASE_URL,
+  movies,
+  selectMovieTitle,
+  form,
+  resetReview,
+  ullistItem,
+  peopleNamesList
+) => {
+  showPeople.addEventListener('click', (e) => {
+    e.preventDefault();
+    peopleNamesList.innerHTML = '';
+    // errorMessage.innerHTML = '';
+
+    fetch(`${BASE_URL}people`)
+      .then((res) => {
+        return res.json();
+      })
+      .then((people) => {
+        console.log(people, selectMovieTitle.value);
+        for (let person of people) {
+          let filmID = person.films;
+          if (filmID.length < 2) {
+            // p = document.createElement('p)')
+            // const errorMessage = document.querySelector('section p.error');
+            console.log('error')
+
+          }
+          for (let film of filmID) {
+            console.log('person=', person.id, person.name, 'film=', film);
+            const getFilm = film.substring(film.lastIndexOf('/') + 1);
+
+            console.log('gettttt=', getFilm, selectMovieTitle.value);
+            if (selectMovieTitle.value === getFilm) {
+              let personList = document.createElement('li');
+              personList.textContent = person.name;
+              peopleNamesList.append(personList);
+            }
+          }
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  });
+};
+
+/********************************/
+/*  RENDERMOVIEDETAILS FUNCTION */
+/*******************************/
+/**
+ *= writeMovoerenders and handles the form to take necessay input infornation
+ * @param {string} BASE_URL -Ghibli app URL
+ * @param { select tag} selectMovieTitle - select options dynamically created  * for select tag
+ * @param {div tag} movieInfo- display the movie details once user selects the * movie
+ * @param {form tag} form - form is used to handle all the input details and   * submit it
+ * @param {button tag} resetReview - a button that resets listitems of ul list
+ * @param {ul tag}  ullistItem - to access list items in the DOM
+ * @param {ol tag} peopleNamesLisst - display the ordered list items
+ * @modifies {string} modifies the DOM
+ * @returns - No returns
+ *
+ */
+
 const writeMovieReviews = (
   movies,
   selectMovieTitle,
   movieInfo,
   form,
-  resetReview
+  resetReview,
+  ullistItem,
+  peopleNamesList
 ) => {
+  movieInfo.innerHTML = '';
+  peopleNamesList.innerHTML = '';
+
   form.addEventListener('submit', (e) => {
     e.preventDefault();
     const errorMessage = document.querySelector('section p.error');
-    errorMessage.innerHTML = ''
+    errorMessage.innerHTML = '';
 
     const reviewInput = e.target.review.value;
     if (reviewInput === '' || reviewInput === null) {
-      
-      console.log(errorMessage);
+      // console.log(errorMessage);
       errorMessage.innerHTML = `Error!!! Reviews cannot be blank.Enter your review`;
       return;
     }
 
     if (selectMovieTitle.value === '' || selectMovieTitle.value === null) {
       if (reviewInput !== '' || reviewInput !== null) {
-        alert("Please select a movie first");
+        alert('Please select a movie first');
       }
-     return;
+      return;
     }
 
     let duplicateListItem = false;
     ul = document.querySelector('ul');
 
-    const listItem = document.querySelectorAll('section ul li');
-    const listArray = Array.from(listItem);
+    ullistItem = document.querySelectorAll('section ul li');
+    const listArray = Array.from(ullistItem);
     listArray.forEach((list) => {
-      console.log(list);
       if (list.textContent.includes(reviewInput)) {
         duplicateListItem = true;
         form.reset();
       }
     });
-
+    let findMovie = movies.filter((ele) => ele.id === selectMovieTitle.value);
+    // console.log(movies);
     if (!duplicateListItem) {
       let li = document.createElement('li');
-      li.innerHTML += `<strong>${selectMovieTitle.value}: </strong><span>${reviewInput}</span>`;
+      li.innerHTML += `<strong>${findMovie[0].title}: </strong><span>${reviewInput}</span>`;
       ul.append(li);
       form.reset();
     }
@@ -56,10 +124,36 @@ const writeMovieReviews = (
   });
 };
 
-const renderMovieDetails = (movies, selectMovieTitle, movieInfo) => {
+/***************************/
+/*  RENDERMOVIEDETAILS FUNCTION */
+/*************************/
+/**
+ * renderMovieDetails renders all the necessary movie description and details
+ * @param {string} BASE_URL -Ghibli app URL
+ * @param { select tag} selectMovieTitle - select options dynamically created  * for select tag
+ * @param {div tag} movieInfo- display the movie details once user selects the * movie
+ * @param {form tag} form - form is used to handle all the input details and   * submit it
+ * @param {button tag} resetReview - a button that resets listitems of ul list
+ * @param {ul tag}  ullistItem - to access list items in the DOM
+ * @param {ol tag} peopleNamesLisst - display the ordered list items
+ * @modifies {string} modifies the DOM
+ * @returns - No return
+ *
+ */
+
+const renderMovieDetails = (
+  movies,
+  selectMovieTitle,
+  movieInfo,
+  form,
+  resetReview,
+  ullistItem,
+  peopleNamesList
+) => {
+  console.log(movies, selectMovieTitle, movieInfo);
   selectMovieTitle.addEventListener('change', (e) => {
     e.preventDefault();
-
+    // console.log(e.target.value);
     movieInfo.innerHTML = '';
     const selectedVal = e.target.value;
     let title = document.createElement('h3');
@@ -67,7 +161,7 @@ const renderMovieDetails = (movies, selectMovieTitle, movieInfo) => {
     let description = document.createElement('p');
 
     for (let movie of movies) {
-      if (selectedVal === movie.title) {
+      if (selectMovieTitle.value === movie.id) {
         title.textContent = movie.title;
         release_date.textContent = movie.release_date;
         description.textContent = movie.description;
@@ -77,12 +171,31 @@ const renderMovieDetails = (movies, selectMovieTitle, movieInfo) => {
   });
 };
 
+/***************************/
+/*  CREATESELECT FUNCTION */
+/*************************/
+/**
+ * createSelect fetches data from the api and calls the other appropraite
+ * functions in the DOM to display a review, gneerate listItems and
+ * @param {string} BASE_URL -Ghibli app URL
+ * @param { select tag} selectMovieTitle - select options dynamically created  * for select tag
+ * @param {div tag} movieInfo- display the movie details once user selects the * movie
+ * @param {form tag} form - form is used to handle all the input details and   * submit it
+ * @param {button tag} resetReview - a button that resets listitems of ul list
+ * @param {ul tag}  ullistItem - to access list items in the DOM
+ * @param {ol tag} peopleNamesLisst - display the ordered list items
+ * @modifies {string} modifies the DOM
+ * @returns - No return
+ */
+
 const createSelect = (
   BASE_URL,
   selectMovieTitle,
   movieInfo,
   form,
-  resetReview
+  resetReview,
+  ullistItem,
+  peopleNamesList
 ) => {
   fetch(`${BASE_URL}films`)
     .then((res) => {
@@ -91,25 +204,74 @@ const createSelect = (
     .then((movies) => {
       movies.forEach((movie) => {
         let option = document.createElement('option');
-        option.value = movie.title;
+        option.value = movie.id;
         option.textContent = movie.title;
         selectMovieTitle.append(option);
       });
-      renderMovieDetails(movies, selectMovieTitle, movieInfo);
-      writeMovieReviews(movies, selectMovieTitle, movieInfo, form, resetReview);
+
+      renderMovieDetails(
+        movies,
+        selectMovieTitle,
+        movieInfo,
+        form,
+        resetReview,
+        ullistItem,
+        peopleNamesList
+      );
+      writeMovieReviews(
+        movies,
+        selectMovieTitle,
+        movieInfo,
+        form,
+        resetReview,
+        ullistItem,
+        peopleNamesList
+      );
+      showPeopleNames(
+        BASE_URL,
+        movies,
+        selectMovieTitle,
+        form,
+        resetReview,
+        ullistItem,
+        peopleNamesList
+      );
     })
     .catch((error) => {
       console.log(error);
     });
 };
 
+/****************/
+/*  MAIN BODY  */
+/**************/
+
 const BASE_URL = 'https://ghibliapi.herokuapp.com/';
 const selectMovieTitle = document.querySelector('section select');
 const movieInfo = document.querySelector('#display-info');
 const form = document.querySelector('form');
 const resetReview = document.querySelector('#reset-reviews');
+const ullistItem = document.querySelectorAll('section ul li');
+const peopleNamesList = document.querySelector('section ol');
+const showPeople = document.querySelector('#show-people');
 
+/***
+ *
+ * DOMCONTENTLOADED function
+ *
+ */
 window.addEventListener('DOMContentLoaded', (event) => {
   console.log('DOM fully loaded and parsed');
-  createSelect(BASE_URL, selectMovieTitle, movieInfo, form, resetReview);
+
+  /* generate select options dynamically **/
+
+  createSelect(
+    BASE_URL,
+    selectMovieTitle,
+    movieInfo,
+    form,
+    resetReview,
+    ullistItem,
+    peopleNamesList
+  );
 });
